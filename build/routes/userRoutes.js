d2141f825c132c2480327d973f9562bc9a9dc2cc
Maria Miguel Icaran <mmicaran@hotmail.com>
2019-11-20 19:52:20 +0100

    crud

--- build/routes/userRoutes.js
+++ build/routes/userRoutes.js
@@ -10,6 +10,27 @@ class userRoutes {
         this.router = express_1.Router();
         this.routes();
     }
+    getUsers(req, res) {
+        User_1.default.find({}).then((data) => {
+            res.status(200).json(data);
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
+    }
+    getUser(req, res) {
+        User_1.default.findOne({ "_id": req.params.id }).then((data) => {
+            res.status(200).json(data);
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
+    }
+    deleteUser(req, res) {
+        User_1.default.findByIdAndDelete({ "_id": req.params.id }).then((data) => {
+            res.status(200).json(data);
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
+    }
     login(req, res) {
         console.log(req.params);
         User_1.default.findOne({ username: req.body.username, password: req.body.password }).then((data) => {
@@ -19,7 +40,6 @@ class userRoutes {
         });
     }
     postUser(req, res) {
-        console.log(req.body.username + req.body.password + req.body.mail);
         const { username, password, mail } = req.body;
         const user = new User_1.default({
             username, password, mail
@@ -27,12 +47,19 @@ class userRoutes {
         user.save().then((data) => {
             res.status(200).json(data);
         }).catch((error) => {
-            res.status(500).json(error);
+            if (error.code == 11000) {
+                res.status(412).json(error);
+            }
+            else
+                res.status(500).json(error);
         });
     }
     routes() {
         this.router.post('/login', this.login);
         this.router.post('/postuser', this.postUser);
+        this.router.get('/getusers', this.getUsers);
+        this.router.get('/getuser/:id', this.getUser);
+        this.router.delete('/deleteuser/:id', this.deleteUser);
     }
 }
 const userroutes = new userRoutes();
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -2,9 +2,7 @@ import { Request, Response, Router } from 'express';
 import TravelGroup from '../models/TravelGroup';
 import User  from '../models/User';
 
-class userRoutes
-
-{
+class userRoutes{
 
     router: Router;
 
@@ -14,49 +12,70 @@ class userRoutes
 
     }
 
-        login(req: Request, res: Response): void {
+    getUsers(req: Request, res: Response): void {
+        User.find({}).then((data) => {
+            res.status(200).json(data);
+
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
+
+    }
+
+    getUser(req: Request, res: Response): void {
+        User.findOne({ "_id": req.params.id }).then((data) => {
+            res.status(200).json(data);
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
+    }
+
+    deleteUser(req: Request, res: Response): void{
+        User.findByIdAndDelete({"_id": req.params.id}).then((data) =>{
+            res.status(200).json(data);
+        }).catch((err)=>{
+            res.status(500).json(err);
+        });
+    }
+
+    login(req: Request, res: Response): void {
         console.log(req.params);
 
         User.findOne({username : req.body.username, password : req.body.password}).then((data)=>{
-                res.status(200).json(data);
+            res.status(200).json(data);
 
         }).catch((error) =>{
             res.status(500).json(error);
-
         })
     }
 
-
-        postUser(req: Request, res: Response): void {
-           console.log(req.body.username + req.body.password + req.body.mail);
-          const {username, password, mail} = req.body;
-           const user = new User({
-                    username, password, mail
-            });
-                user.save().then((data) =>{
-                    res.status(200).json(data)
-                    
-                }).catch((error) =>{
+    postUser(req: Request, res: Response): void {
+        const {username, password, mail} = req.body;
+        const user = new User({
+            username, password, mail
+        });
+        user.save().then((data) =>{
+            res.status(200).json(data)
+            
+        }).catch((error) =>{
+            if(error.code == 11000){
+                res.status(412).json(error);
+            }else
                 res.status(500).json(error);
-    
-            });
-        }
-    
-
-
-
-
-
-
-        routes()
-        {
-            this.router.post('/login', this.login);
-            this.router.post('/postuser', this.postUser);
-        }
+        });
     }
+
+    routes(){
+        this.router.post('/login', this.login);
+        this.router.post('/postuser', this.postUser);
+        this.router.get('/getusers', this.getUsers);
+        this.router.get('/getuser/:id', this.getUser);        
+        this.router.delete('/deleteuser/:id', this.deleteUser);
+    }
+}
     
-        const userroutes = new userRoutes();
-        userroutes.routes();
-    
-        export default userroutes.router;
+const userroutes = new userRoutes();
+userroutes.routes();
+
+export default userroutes.router;
     
